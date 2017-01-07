e2b0e9648f85ac3c756920ad582963e3d367c3c5
BobDoleOwndU <spikey5556@hotmail.com>
2017-01-06 23:31:19 -0500

    Initial commit

---
+++ QuickHash/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace QuickHash
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textLabel = new System.Windows.Forms.Label();
+            this.textTextBox = new System.Windows.Forms.TextBox();
+            this.hashLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textLabel
+            // 
+            this.textLabel.AutoSize = true;
+            this.textLabel.Location = new System.Drawing.Point(13, 13);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(51, 13);
+            this.textLabel.TabIndex = 0;
+            this.textLabel.Text = "File Path:";
+            // 
+            // textTextBox
+            // 
+            this.textTextBox.Location = new System.Drawing.Point(13, 30);
+            this.textTextBox.Name = "textTextBox";
+            this.textTextBox.Size = new System.Drawing.Size(259, 20);
+            this.textTextBox.TabIndex = 1;
+            // 
+            // hashLabel
+            // 
+            this.hashLabel.AutoSize = true;
+            this.hashLabel.Location = new System.Drawing.Point(13, 57);
+            this.hashLabel.Name = "hashLabel";
+            this.hashLabel.Size = new System.Drawing.Size(35, 13);
+            this.hashLabel.TabIndex = 2;
+            this.hashLabel.Text = "Hash:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(259, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 143);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.hashLabel);
+            this.Controls.Add(this.textTextBox);
+            this.Controls.Add(this.textLabel);
+            this.Name = "Form1";
+            this.Text = "QuickHash";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label textLabel;
+        private System.Windows.Forms.TextBox textTextBox;
+        private System.Windows.Forms.Label hashLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
